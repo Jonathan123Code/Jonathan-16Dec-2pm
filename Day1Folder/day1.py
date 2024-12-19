379e8900d239a2668a1031238c38c6f29fbc39e7
Jonathan123Code <syjonathan@hotmail.com>
2024-12-19 07:16:28 +0000

    PythonMinecraftCamp

--- Day1Folder/day1.py
+++ Day1Folder/day1.py
@@ -1,4 +1,28 @@
 # Write all your codes for Day 1 here.
 # REMEMBER to change main.py to import this file.
 # You can COMMENT out the previous task before going on to the next task
-print("hello from day1")
+# print("My name is Sy Jonathan")
+# print("I am 12 years old")
+# print("I attend Townsville Primary School")
+# print("If I had $2000, I would treat my friends and family to a meal")
+# animal1 = "Cat"
+# animal2 = "Dog"
+# animal3 = "Tiger"
+# print(animal1)
+# print(animal2)
+# print(animal3)
+# name = "Jonathan"
+# food = "sushi"
+# print("My name is " + name)
+# print("My favourite food is " + food)
+# def intro():
+#     print("My name is Sy Jonathan")
+#     print("I am 12 years old")
+#     print("I attend Townsville Primary School")
+#     print("If I had $2000, I would treat my friends and family to a meal")
+# intro()
+# def addition(num1,num2):
+#     print(num1 + num2)
+# addition(2,3)
+# addition(243,236)
+# addition(5000,3)
--- Day4Folder/day4.py
+++ Day4Folder/day4.py
@@ -1,4 +1,30 @@
 # Write all your codes for Day 4 here.
 # REMEMBER to change main.py to import this file.
 # You can COMMENT out the previous task before going on to the next task
-print("hello from day4")
+# print("hello from day4")
+# counter1 = 0
+# while counter1 < 10:
+#     print(counter1)
+#     counter1 = counter1 + 1
+# counter2 = 5
+# while counter2 < 33:
+#     print(counter2)
+#     counter2 = counter2 + 1
+# counter3 = 50
+# while counter3 > 0:
+#     print(counter3)
+#     counter3 = counter3 - 1
+# riddleAns = "A hole"
+# print("A barrel of water weighed 60 pounds.")
+# userAns = input("Someone put something in it and now it weighs 40 pounds. What did the person add? ")
+# counter = 1
+# while userAns != riddleAns:
+#     if counter == 5:
+#         print("Hint: It is a barrel of WATER.")
+#     if counter == 10:
+#         print("Hint: How to you pop a balloon")
+#     if counter == 15:
+#         print("Hint: Answer = A hole")
+#     counter = counter + 1
+#     print("A barrel of water weighed 60 pounds.")
+#     userAns = input("Someone put something in it and now it weighs 40 pounds. What did the person add? ")
